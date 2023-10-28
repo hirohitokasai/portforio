040da9f087cefaae0b6815f13e0a7d6c47898e08
hirohitokasai <148047417+hirohitokasai@users.noreply.github.com>
2023-10-28 15:57:41 +0900

    test3

--- script/main.js
+++ script/main.js
@@ -1,1 +1,13 @@
-$(function(){$(".nav_toggle").click(function(){$(".nav_toggle").toggleClass("menu_icon_animate");$("nav").toggleClass("show2");$(".main-p").toggleClass("background")});$(".gotop").click(function(){$("body,html").animate({scrollTop:0},500);return false})});
+$(function() {
+    $('.nav_toggle').click(function() {
+        $('.nav_toggle').toggleClass("menu_icon_animate");
+        $('nav').toggleClass("show2");
+        $('.main-p').toggleClass("background");
+    });
+
+    $(".gotop").click(function(){
+        $("body,html").animate({scrollTop: 0}, 500);
+        return false;
+    })
+});
+
